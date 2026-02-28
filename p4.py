70a8c0e53d8beca9f733e72716cdf979fe263a9d
Nathan Nail <nailcnathan@gmail.com>
2026-02-27 17:07:57 -0700

    prototype for 4 complete. Might run on leda

--- p4.py
+++ p4.py
@@ -25,7 +25,20 @@ my_svc = mySVC(eta = 0.001, n_iter = 50, random_state=100)
 sk_primal = skSVC(loss = "hinge", dual = False)
 sk_dual = skSVC(loss = "hinge", dual = True)
 
-models = {my_svc : [], sk_primal : [], sk_dual : []}
+models = {"my_svc" : [], "sk_primal" : [], "sk_dual" : []}
+
+def fit_sklearn_model(X_, y_, n_iter_, dual_ : bool):
+    for i in range(1, n_iter_ + 1):
+        model = skSVC(loss = "hinge", dual = dual_)
+        model.fit(X = X_, y = y_, n_iter = i)
+
+        result = model.predict(X_)
+        accuracy = model.score(X_, y_)
+
+        if dual_ == False:
+            models.get("sk_primal").append(accuracy)
+        else:
+            models.get("sk_dual").append(accuracy)
 
 datasets = []   
 for name in filenames:
@@ -33,7 +46,7 @@ for name in filenames:
     datasets.append(data)
 
 
-for model in models:
+for model in models.keys():
     print(str(model))
     for data in datasets:
         n_samples = data.shape[0]
@@ -41,10 +54,25 @@ for model in models:
         X_data = data[:, 0:n_dims - 1]
         y_data = data[:, n_dims - 1]
         fit_start = time.clock_gettime(5)
-        model.fit(X_data, y_data)
+        
+        if model == "my_svc":
+            mySVC.fit(X_data, y_data)
+        elif model == "sk_primal":
+            fit_sklearn_model(X_data, y_data, n_iter = 50, dual = False)
+        elif model == "sk_dual":
+            fit_sklearn_model(X_data, y_data, n_iter = 50, dual = False)
+
         fit_end = time.clock_gettime(5)
         print("%d dims, %d samples: %f" % (n_dims, n_samples, fit_end - fit_start))
         end_weight = my_svc.w_
         models.get(model).append(model.errors_)
+
+
+
+
+
+        
+
+
 
 
