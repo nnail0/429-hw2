2d45bc93a6985e447149a54ff9d3dbc768848d16
Nathan Nail <nnail0@unm.edu>
2026-02-27 18:20:30 -0700

    changes to p4.py

--- p4.py
+++ p4.py
@@ -10,16 +10,34 @@ import time
 from matplotlib import pyplot as plt
 
 filenames = [
-    "10d_500n.csv",
-    "10d_5000n.csv"
-    "10d_50000n.csv",
-    "50d_500n.csv",
-    "50d_5000n.csv",
-    "50d_50000n.csv",
-    "100d_500n.csv",
-    "100d_5000n.csv",
-    "100d_50000n.csv"
+    "10d_500n_train.csv",
+    "10d_500n_test.csv",
+    "10d_5000n_train.csv",
+    "10d_5000n_test.csv",
+    "10d_50000n_train.csv",
+    "10d_50000n_test.csv",
+    "50d_500n_train.csv",
+    "50d_500n_test.csv",
+    "50d_5000n_train.csv",
+    "50d_5000n_test.csv",
+    "50d_50000n_train.csv",
+    "50d_50000n_test.csv",
+    "100d_500n_train.csv",
+    "100d_500n_test.csv",
+    "100d_5000n_train.csv",
+    "100d_5000n_test.csv",
+    "100d_50000n_train.csv",
+    "100d_50000n_test.csv"
 ]
+
+train_filenames = []
+test_filenames = []
+
+for name in filenames:
+    if "train" in name:
+        train_filenames.append(name)
+    else:
+        test_filenames.append(name)
 
 my_svc = mySVC(eta = 0.001, n_iter = 50, random_state=100)
 sk_primal = skSVC(loss = "hinge", dual = False)
@@ -40,39 +58,38 @@ def fit_sklearn_model(X_, y_, n_iter_, dual_ : bool):
         else:
             models.get("sk_dual").append(accuracy)
 
-datasets = []   
-for name in filenames:
-    data = np.genfromtxt(name, delimiter=',')
-    datasets.append(data)
+train_datasets = []
+test_datasets = []
+for name in train_filenames:
+    train_data = np.genfromtxt(name, delimiter=',')
+    train_datasets.append(train_data)
 
+for name in test_filenames:
+    test_data = np.genfromtxt(name, delimiter = ",")
+    test_datasets.append(test_data)
 
 for model in models.keys():
     print(str(model))
-    for data in datasets:
+    print("\n")
+    for data in train_datasets:
         n_samples = data.shape[0]
         n_dims = data.shape[1]
         X_data = data[:, 0:n_dims - 1]
         y_data = data[:, n_dims - 1]
         fit_start = time.clock_gettime(5)
+
         
         if model == "my_svc":
             mySVC.fit(X_data, y_data)
+            models.get(model).update({"my_svc": model.errors_})
         elif model == "sk_primal":
-            fit_sklearn_model(X_data, y_data, n_iter = 50, dual = False)
+            result = fit_sklearn_model(X_data, y_data, n_iter = 50, dual = False)
+            models.get(model).update({"sk_primal" : result})
         elif model == "sk_dual":
-            fit_sklearn_model(X_data, y_data, n_iter = 50, dual = False)
+            result = fit_sklearn_model(X_data, y_data, n_iter = 50, dual = True)
+            models.get(model).update({"sk_dual" : result})
 
         fit_end = time.clock_gettime(5)
         print("%d dims, %d samples: %f" % (n_dims, n_samples, fit_end - fit_start))
-        end_weight = my_svc.w_
-        models.get(model).append(model.errors_)
-
-
-
-
-
-        
-
-
-
+        # end_weight = my_svc.w_
 
